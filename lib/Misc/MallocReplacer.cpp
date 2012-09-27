ec1fb6380f9d92753e0845a60cc8869d9110287e
Jingyue Wu <jingyue@cs.columbia.edu>
2012-09-27 13:53:53 -0400

    Added a malloc replacer

---
+++ lib/Misc/MallocReplacer.cpp
@@ -0,0 +1,61 @@
+// Author: Jingyue
+
+#include "llvm/Pass.h"
+#include "llvm/Module.h"
+#include "llvm/Instructions.h"
+#include "llvm/Support/CallSite.h"
+
+using namespace llvm;
+
+namespace rcs {
+struct MallocReplacer: public BasicBlockPass {
+  static char ID;
+
+  MallocReplacer(): BasicBlockPass(ID) {
+    MyMalloc = Malloc = NULL;
+  }
+  virtual bool runOnBasicBlock(BasicBlock &BB);
+  virtual bool doInitialization(Module &M);
+  virtual bool doInitialization(Function &F) {
+    return BasicBlockPass::doInitialization(F);
+  }
+
+ private:
+  Function *MyMalloc, *Malloc;
+};
+}
+
+using namespace rcs;
+
+char MallocReplacer::ID = 0;
+
+static RegisterPass<MallocReplacer> X("replace-malloc",
+                                      "Replace malloc with my_malloc",
+                                      false,
+                                      false);
+
+bool MallocReplacer::doInitialization(Module &M) {
+  assert(M.getFunction("my_malloc") == NULL);
+  Malloc = M.getFunction("malloc");
+  if (Malloc) {
+    MyMalloc = Function::Create(Malloc->getFunctionType(),
+                                GlobalValue::ExternalLinkage,
+                                "my_malloc",
+                                &M);
+  }
+  return true;
+}
+
+bool MallocReplacer::runOnBasicBlock(BasicBlock &BB) {
+  if (Malloc == NULL) {
+    // nothing to replace
+    return false;
+  }
+  for (BasicBlock::iterator Ins = BB.begin(); Ins != BB.end(); ++Ins) {
+    CallSite CS(Ins);
+    if (CS && CS.getCalledFunction() == Malloc) {
+      CS.setCalledFunction(MyMalloc);
+    }
+  }
+  return true;
+}
