db8648506de6cf7d9e49f973d22850352174be94
Jingyue Wu <wujingyue@gmail.com>
2012-10-28 16:51:58 -0400

    Extra LLVM version to rcs/config.h

---
+++ AutoRegen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+die () {
+	echo "$@" 1>&2
+	exit 1
+}
+
+test -f configure.ac || die "Can't find 'autoconf' dir; please cd into it first"
+
+autoconf --version | egrep '2\.[56][0-9]' > /dev/null
+if test $? -ne 0 ; then
+	die "Your autoconf was not detected as being 2.5x or 2.6x"
+fi
+
+echo "Regenerating aclocal.m4 with aclocal"
+rm -f aclocal.m4
+aclocal -I `llvm-config --src-root`/autoconf/m4 || die "aclocal failed"
+
+echo "Regenerating configure with autoconf"
+autoconf --warnings=all -o configure configure.ac || die "autoconf failed"
+
+echo "Regenerating configure header files with autoheader"
+autoheader configure.ac || die "autoheader failed"
+
+exit 0
