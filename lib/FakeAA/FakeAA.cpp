8af2b6d720cca344aebd4efd1e808fb78500a7d4
Jingyue Wu <wujingyue@gmail.com>
2013-01-22 10:31:50 -0500

    Unimportant changes. FakeAA is fake anyway.

--- lib/FakeAA/FakeAA.cpp
+++ lib/FakeAA/FakeAA.cpp
@@ -3,43 +3,50 @@
 #include "llvm/Analysis/AliasAnalysis.h"
 using namespace llvm;
 
+#include "rcs/IDAssigner.h"
+using namespace rcs;
+
 namespace rcs {
-struct FakeAA: public ModulePass, public AliasAnalysis {
+struct FakeAA: public ImmutablePass, public AliasAnalysis {
   static char ID;
 
-  FakeAA(): ModulePass(ID) {}
-  virtual bool runOnModule(Module &M);
+  FakeAA(): ImmutablePass(ID) {}
+  virtual void initializePass();
   virtual void getAnalysisUsage(AnalysisUsage &AU) const;
   virtual void *getAdjustedAnalysisPointer(AnalysisID PI);
-  virtual AliasResult alias(const Location &L1, const Location &L2);
+
+  // interfaces of AliasAnalysis
+  virtual AliasResult alias(const Location &L1, const Location &L2) {
+    IDAssigner &IDA = getAnalysis<IDAssigner>();
+    errs() << "FakeAA::alias " << IDA.getNumValues() << "\n";
+    return NoAlias;
+  }
+  virtual ModRefBehavior getModRefBehavior(const Function *F) { return DoesNotAccessMemory; }
+  virtual ModRefBehavior getModRefBehavior(ImmutableCallSite CS) { return DoesNotAccessMemory; }
+  virtual bool pointsToConstantMemory(const Location &Loc, bool OrLocal = false) { return true; }
+  virtual void deleteValue(Value *V) {}
+  virtual void copyValue(Value *From, Value *To) {}
+  virtual void addEscapingUse(Use &U) {}
 };
 }
-using namespace rcs;
 
 static RegisterPass<FakeAA> X("fake-aa", "Fake AA", false, true);
 static RegisterAnalysisGroup<AliasAnalysis> Y(X);
 
 char FakeAA::ID = 0;
 
-bool FakeAA::runOnModule(Module &M) {
-  errs() << "FakeAA::runOnModule\n";
+void FakeAA::initializePass() {
+  DEBUG(dbgs() << "FakeAA::initializePass\n";);
   InitializeAliasAnalysis(this);
-  return false;
 }
 
 void FakeAA::getAnalysisUsage(AnalysisUsage &AU) const {
   AliasAnalysis::getAnalysisUsage(AU);
-  AU.setPreservesAll();
+  AU.addRequired<IDAssigner>();
 }
 
 void *FakeAA::getAdjustedAnalysisPointer(AnalysisID PI) {
   if (PI == &AliasAnalysis::ID)
     return (AliasAnalysis *)this;
   return this;
-}
-
-AliasAnalysis::AliasResult FakeAA::alias(const Location &L1,
-                                         const Location &L2) {
-  errs() << "FakeAA::alias\n";
-  return AliasAnalysis::NoAlias;
 }
